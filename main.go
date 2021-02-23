7b4e36ee297e9caa67e8eb5a9d3ad100ac8883a0
reisinger <peter.reisinger@form3.tech>
2021-02-23 16:35:19 +0000

    add new lines to error messages

--- main.go
+++ main.go
@@ -30,13 +30,13 @@ func main() {
 	// cidrsubnet
 	if len(args) == 3 {
 		if err := CIDRSubnet(ip, ipnet, toInt(args[1]), toInt(args[2])); err != nil {
-			fmt.Printf("cidr subnet: %v", err)
+			fmt.Printf("cidr subnet: %v\n", err)
 			os.Exit(1)
 		}
 		return
 	}
 
-	fmt.Printf("received %d arguments, provide 1 (CIDR) or 3 (CIDR, newbits, netmum)", len(args))
+	fmt.Printf("received %d arguments, provide 1 (CIDR) or 3 (CIDR, newbits, netmum)\n", len(args))
 	os.Exit(1)
 }
 
@@ -44,7 +44,7 @@ func toInt(s string) int {
 
 	out, err := strconv.Atoi(s)
 	if err != nil {
-		fmt.Printf("cannot convert %s to int", s)
+		fmt.Printf("cannot convert %s to int\n", s)
 		os.Exit(1)
 	}
 	return out
